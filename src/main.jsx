09ba608164636eca51818d3c97c20a8a8514e5d5
tanjid-showkot <tanjidsaykot611@gmail.com>
2025-09-23 01:36:37 +0600

    pwaBugFix

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import webRouter from "./Router/Router.jsx";
 import { Context } from "./Context/Context.jsx";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New version available. Reload?")) {
+      updateSW();
+    }
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <Context>
